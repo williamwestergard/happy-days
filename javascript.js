7e3f2781c722b141caf41793dcc74b06f0af45e2
williamwestergard <williamwestergardd@gmail.com>
2024-11-14 00:38:42 +0100

    update pc 5

--- javascript.js
+++ javascript.js
@@ -10,3 +10,25 @@ function hideMusicLinks(){
 }
 
 
+let audio = document.getElementById("songs-teaser");
+
+function playAudio() {
+    audio.play();
+    document.getElementById("pause-button").style.display ="block";
+    document.getElementById("pause-button").style.transition =".s";
+    document.getElementById("pause-image").style.display ="block";
+    document.getElementById("play-image").style.display ="none";
+}
+
+function pauseAudio() {
+    audio.pause();
+    document.getElementById("pause-button").style.display ="none";
+    document.getElementById("play-image").style.display ="block";
+}
+
+
+audio.addEventListener("ended", (event) => {
+    document.getElementById("pause-button").style.display ="none";
+    document.getElementById("play-image").style.display ="block"; 
+});
+

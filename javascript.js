68ffea150ca475bb171ebc485fb0a61c0df0801f
williamwestergard <williamwestergardd@gmail.com>
2024-11-13 18:07:53 +0100

    laptop uppdatering nästan klar

---
+++ javascript.js
@@ -0,0 +1,10 @@
+
+function showMusicLinks() {
+    document.getElementById("main-content").style.display = "none";
+    document.getElementById("music-section").style.display = "block";
+}
+
+function hideMusicLinks(){
+    document.getElementById("main-content").style.display = "block";
+    document.getElementById("music-section").style.display = "none";
+}

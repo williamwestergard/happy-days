825ca2d62729661f1ef4c24864bd3a1e0ee01a9f
williamwestergard <williamwestergardd@gmail.com>
2024-11-14 23:22:52 +0100

    added a gitignore file

---
+++ javascript.js
@@ -0,0 +1,41 @@
+
+function showMusicLinks() {
+    document.getElementById("main-content").style.display = "none";
+    document.getElementById("music-section").style.display = "block";
+}
+
+function hideMusicLinks(){
+    document.getElementById("main-content").style.display = "block";
+    document.getElementById("music-section").style.display = "none";
+}
+
+
+let audio = document.getElementById("songs-teaser");
+
+function playAudio() {
+    audio.play();
+    document.getElementById("pause-button").style.display ="block";
+    document.getElementById("pause-button").style.transition =".s";
+    document.getElementById("pause-image").style.display ="block";
+    document.getElementById("play-image").style.display ="none";
+}
+
+function pauseAudio() {
+    audio.pause();
+    document.getElementById("pause-button").style.display ="none";
+    document.getElementById("play-image").style.display ="block";
+}
+
+
+audio.addEventListener("ended", (event) => {
+    document.getElementById("pause-button").style.display ="none";
+    document.getElementById("play-image").style.display ="block"; 
+});
+
+
+function stopMusic() {
+    audio.pause();
+    document.getElementById("pause-button").style.display ="none";
+    document.getElementById("play-image").style.display ="block"; 
+}
+
